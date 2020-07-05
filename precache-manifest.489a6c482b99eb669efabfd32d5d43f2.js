f534ec0e4587db633703d5df3dbfcd5b10496b3c
Juyoung Kim <kjyzz43@naver.com>
2020-07-05 20:32:43 +0900

    Updates

---
+++ precache-manifest.489a6c482b99eb669efabfd32d5d43f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402aab3f7e2013834d28f530806728f2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cfc74d7386da6474259d",
+    "url": "/my-app/static/css/main.62004cc9.chunk.css"
+  },
+  {
+    "revision": "3cf08d808cb665e73216",
+    "url": "/my-app/static/js/2.618f778f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.618f778f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc74d7386da6474259d",
+    "url": "/my-app/static/js/main.1cb224b4.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
